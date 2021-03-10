493d972c8b7b5bf2e10e47557b27759f8ea260d5
Jaya <Jaya@DESKTOP-19OL47G>
2021-03-10 10:17:49 +0700

    initial commit

---
+++ Github1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Github1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstnum;
+            double secondnum;
+            double answer;
+            string ops;
+
+            Console.WriteLine("___________________________________");
+            Console.WriteLine("|                                 |");
+            Console.WriteLine("|           Aditya Wijaya         |");
+            Console.WriteLine("|_________________________________|");
+        }
+
+    }
+}
