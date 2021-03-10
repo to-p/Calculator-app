7958dcfdb7c19003d7786f54aad8bac16593705c
Jaya <Jaya@DESKTOP-19OL47G>
2021-03-10 10:23:11 +0700

    initial commit

--- Github1/Program.cs
+++ Github1/Program.cs
@@ -19,6 +19,7 @@ namespace Github1
             Console.WriteLine("|                                 |");
             Console.WriteLine("|           Aditya Wijaya         |");
             Console.WriteLine("|_________________________________|");
+
         }
 
     }
